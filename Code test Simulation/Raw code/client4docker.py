f08056c6cefe62827650ee8a92744d29fd730230
phonghongs <phonghongs@gmail.com>
2021-11-11 11:02:59 +0700

    docker_fix : current speed and angle

--- Code test Simulation/Raw code/client4docker.py
+++ Code test Simulation/Raw code/client4docker.py
@@ -26,27 +26,26 @@ try:
         message_getState = bytes("0", "utf-8")
         s.sendall(message_getState)
         state_date = s.recv(100)
-        current_speed, current_angle = state_date.decode("utf-8").split(' ')
+
+        try:
+            current_speed, current_angle = state_date.decode(
+                "utf-8"
+                ).split(' ')
+        except Exception as er:
+            print(er)
+            pass
 
         message = bytes(f"1 {sendBack_angle} {sendBack_Speed}", "utf-8")
         s.sendall(message)
         data = s.recv(100000)
 
-        """
-        - Chương trình đưa cho bạn 1 giá trị đầu vào:
-            * image: hình ảnh trả về từ xe
-            * current_speed: vận tốc hiện tại của xe
-            * current_angle: góc bẻ lái hiện tại của xe
-
-        - Bạn phải dựa vào giá trị đầu vào này để tính toán và gán lại góc lái và tốc độ xe vào 2 biến:
-            * Biến điều khiển: sendBack_angle, sendBack_Speed
-            Trong đó:
-                + sendBack_angle (góc điều khiển): [-25, 25]  NOTE: ( âm là góc trái, dương là góc phải)
-                + sendBack_Speed (tốc độ điều khiển): [-150, 150] NOTE: (âm là lùi, dương là tiến)
-        """
-
         try:
-            image = cv2.imdecode(np.frombuffer(data, np.uint8), -1)
+            image = cv2.imdecode(
+                np.frombuffer(
+                    data,
+                    np.uint8
+                    ), -1
+                )
 
             print(current_speed, current_angle)
             print(image.shape)
