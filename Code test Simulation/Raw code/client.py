9245c57d42552b02d099944b345abfe880e1b4dc
phonghongs <phonghongs@gmail.com>
2021-11-11 12:58:16 +0700

    Update: new format code, update client4docker.py

--- Code test Simulation/Raw code/client.py
+++ Code test Simulation/Raw code/client.py
@@ -1,55 +1,86 @@
 # Import socket module
 import socket
-import time
 import cv2
 import numpy as np
+
+global sendBack_angle, sendBack_Speed, current_speed, current_angle
+sendBack_angle = 0
+sendBack_Speed = 0
+current_speed = 0
+current_angle = 0
 
 # Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Define the port on which you want to connect
-port = 54321
+PORT = 54321
 # connect to the server on local computer
-s.connect(('127.0.0.1', port))
+s.connect(('127.0.0.1', PORT))
 
-pre = time.time()
 
-sendBack_angle = 0
-sendBack_Speed = 0
+def Control(angle, speed):
+    global sendBack_angle, sendBack_Speed
+    sendBack_angle = angle
+    sendBack_Speed = speed
 
-try:
-    while True:
-        # Send data
-        message_getState = bytes("0", "utf-8")
-        s.sendall(message_getState)
-        state_date = s.recv(100)
-        current_speed, current_angle = state_date.decode("utf-8").split(' ')
 
-        message = bytes(f"1 {sendBack_angle} {sendBack_Speed}", "utf-8")
-        s.sendall(message)
-        data = s.recv(100000)
+if __name__ == "__main__":
+    try:
+        while True:
 
-        image = cv2.imdecode(np.frombuffer(data, np.uint8), -1)
-        """
-        - Chương trình đưa cho bạn 1 giá trị đầu vào:
-            * image: hình ảnh trả về từ xe
-            * current_speed: vận tốc hiện tại của xe
-            * current_angle: góc bẻ lái hiện tại của xe
+            """
+            - Chương trình đưa cho bạn 1 giá trị đầu vào:
+                * image: hình ảnh trả về từ xe
+                * current_speed: vận tốc hiện tại của xe
+                * current_angle: góc bẻ lái hiện tại của xe
 
-        - Bạn phải dựa vào giá trị đầu vào này để tính toán và gán lại góc lái và tốc độ xe vào 2 biến:
-            * Biến điều khiển: sendBack_angle, sendBack_Speed
-            Trong đó:
-                + sendBack_angle (góc điều khiển): [-25, 25]  NOTE: ( âm là góc trái, dương là góc phải)
-                + sendBack_Speed (tốc độ điều khiển): [-150, 150] NOTE: (âm là lùi, dương là tiến)
-        """
+            - Bạn phải dựa vào giá trị đầu vào này để tính toán và
+            gán lại góc lái và tốc độ xe vào 2 biến:
+                * Biến điều khiển: sendBack_angle, sendBack_Speed
+                Trong đó:
+                    + sendBack_angle (góc điều khiển): [-25, 25]
+                        NOTE: ( âm là góc trái, dương là góc phải)
 
-        # your process here
-        cv2.imshow("IMG", image)
+                    + sendBack_Speed (tốc độ điều khiển): [-150, 150]
+                        NOTE: (âm là lùi, dương là tiến)
+            """
 
-        cv2.waitKey(1)
-        sendBack_angle = 100
-        sendBack_Speed = 50
+            message_getState = bytes("0", "utf-8")
+            s.sendall(message_getState)
+            state_date = s.recv(100)
 
-finally:
-    print('closing socket')
-    s.close()
+            try:
+                current_speed, current_angle = state_date.decode(
+                    "utf-8"
+                    ).split(' ')
+            except Exception as er:
+                print(er)
+                pass
+
+            message = bytes(f"1 {sendBack_angle} {sendBack_Speed}", "utf-8")
+            s.sendall(message)
+            data = s.recv(100000)
+
+            try:
+                image = cv2.imdecode(
+                    np.frombuffer(
+                        data,
+                        np.uint8
+                        ), -1
+                    )
+
+                print(current_speed, current_angle)
+                print(image.shape)
+                # your process here
+                cv2.imshow("IMG", image)
+                cv2.waitKey(1)
+                # Control(angle, speed)
+                Control(25, 100)
+
+            except Exception as er:
+                print(er)
+                pass
+
+    finally:
+        print('closing socket')
+        s.close()
--- Code test Simulation/Raw code/client4docker.py
+++ Code test Simulation/Raw code/client4docker.py
@@ -1,62 +1,85 @@
-
 # Import socket module
 import socket
-import time
 import cv2
 import numpy as np
 
+global sendBack_angle, sendBack_Speed, current_speed, current_angle
+sendBack_angle = 0
+sendBack_Speed = 0
+current_speed = 0
+current_angle = 0
 
 # Create a socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Define the port on which you want to connect
-port = 54321
-
+PORT = 54321
 # connect to the server on local computer
-s.connect(('host.docker.internal', port))
+s.connect(('host.docker.internal', PORT))
 
-pre = time.time()
 
-sendBack_angle = 0
-sendBack_Speed = 0
+def Control(angle, speed):
+    global sendBack_angle, sendBack_Speed
+    sendBack_angle = angle
+    sendBack_Speed = speed
 
-try:
-    while True:
-        # Send data
-        message_getState = bytes("0", "utf-8")
-        s.sendall(message_getState)
-        state_date = s.recv(100)
 
-        try:
-            current_speed, current_angle = state_date.decode(
-                "utf-8"
-                ).split(' ')
-        except Exception as er:
-            print(er)
-            pass
+if __name__ == "__main__":
+    try:
+        while True:
 
-        message = bytes(f"1 {sendBack_angle} {sendBack_Speed}", "utf-8")
-        s.sendall(message)
-        data = s.recv(100000)
+            """
+            - Chương trình đưa cho bạn 1 giá trị đầu vào:
+                * image: hình ảnh trả về từ xe
+                * current_speed: vận tốc hiện tại của xe
+                * current_angle: góc bẻ lái hiện tại của xe
 
-        try:
-            image = cv2.imdecode(
-                np.frombuffer(
-                    data,
-                    np.uint8
-                    ), -1
-                )
+            - Bạn phải dựa vào giá trị đầu vào này để tính toán và
+            gán lại góc lái và tốc độ xe vào 2 biến:
+                * Biến điều khiển: sendBack_angle, sendBack_Speed
+                Trong đó:
+                    + sendBack_angle (góc điều khiển): [-25, 25]
+                        NOTE: ( âm là góc trái, dương là góc phải)
 
-            print(current_speed, current_angle)
-            print(image.shape)
-            # your process here
+                    + sendBack_Speed (tốc độ điều khiển): [-150, 150]
+                        NOTE: (âm là lùi, dương là tiến)
+            """
 
-            sendBack_angle = 0
-            sendBack_Speed = 0
+            message_getState = bytes("0", "utf-8")
+            s.sendall(message_getState)
+            state_date = s.recv(100)
 
-        except Exception as er:
-            print(er)
-            pass
-finally:
-    print('closing socket')
-    s.close()
+            try:
+                current_speed, current_angle = state_date.decode(
+                    "utf-8"
+                    ).split(' ')
+            except Exception as er:
+                print(er)
+                pass
+
+            message = bytes(f"1 {sendBack_angle} {sendBack_Speed}", "utf-8")
+            s.sendall(message)
+            data = s.recv(100000)
+
+            try:
+                image = cv2.imdecode(
+                    np.frombuffer(
+                        data,
+                        np.uint8
+                        ), -1
+                    )
+
+                print(current_speed, current_angle)
+                print(image.shape)
+                # your process here
+
+                # Control(angle, speed)
+                Control(25, 100)
+
+            except Exception as er:
+                print(er)
+                pass
+
+    finally:
+        print('closing socket')
+        s.close()
